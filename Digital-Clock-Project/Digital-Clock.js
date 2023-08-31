217cb8bca481e7df3b3fd7b20ff191ad508892b2
ErkyHanma <yenzel.bt@gmail.com>
2023-08-31 16:22:24 -0400

    Add feature to change to 12 hour format

--- Digital-Clock-Project/Digital-Clock.js
+++ Digital-Clock-Project/Digital-Clock.js
@@ -15,19 +15,70 @@ function updateBackgroundAndAmPmClasses(hour) {
   document.querySelector('.ampm-js').innerHTML = ampm;
 }
 
+let timeoutId;
+
 function calculateTime() {
+
+  
   const today = new Date();
   const day = weekDays[today.getDay()];
   const hours = today.getHours();
   const minutes = today.getMinutes();
   const time = `${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
 
+  
+
   document.querySelector('.time-js').innerHTML = time;
   document.querySelector('.day-js').innerHTML = day;
 
   updateBackgroundAndAmPmClasses(hours);
 
-  setTimeout(calculateTime, 200);
+  timeoutId = setTimeout(calculateTime, 200);
+
+  
 }
 
+
+document.querySelector('.switch-js')
+ .addEventListener('click', changeHourFormat);
+
+ let hourFormat = false;
+
+function changeHourFormat() {
+  const today = new Date();
+  let hours = today.getHours();
+  const minutes = today.getMinutes();
+
+    if (!hourFormat) {
+      clearTimeout(timeoutId); 
+      hourFormat = true;
+    } else {
+      calculateTime();
+      hourFormat = false;
+    }
+
+    if(hours >= 12 ) {
+      hours = hours - 12
+      const time2 = `${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+      document.querySelector('.time-js').innerHTML = time2;
+      console.log(time2)
+    } 
+
+    const body = document.body;
+    const ampm = hours <= 12 ? "Am" : "Pm";
+
+    if (ampm === "Am") {
+      body.classList.remove("image-background-night");
+      body.classList.add("image-background-day");
+    } else {
+      body.classList.remove("image-background-day");
+      body.classList.add("image-background-night");
+    }
+
+   
+
+  }
+
+
 window.addEventListener('load', calculateTime);
+
